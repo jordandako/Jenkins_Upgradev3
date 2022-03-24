88e527fe8f6615fbaac807cd465aea1959a22431
Jordan Arizanov <jordan.arizanov@auroralabs.com>
2022-03-24 10:16:22 +0100

    Fix the error first test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,10 +41,5 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-  @Test
-  public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, ";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
+  
 }
